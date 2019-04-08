405ad4e532ec63bbb6dbfa8a3957f765766b212f
CleaveItToBeaver <49290707+CleaveItToBeaver@users.noreply.github.com>
2019-04-08 09:42:37 -0400

    Class and combat changes
    Added player and mook classes, adjusted combat a bit to use the class attributes,

--- skillRoll.py
+++ skillRoll.py
@@ -1,15 +1,36 @@
 import random
 
+class player:
+    name = "Player"
+    ST = 10
+    DX = 10
+    IQ = 10
+    HT = 10
+    maxHP = ST
+    tempHP = maxHP
+
+class baseNPC:
+    name = "mook"
+    ST = 10
+    DX = 10
+    IQ = 10
+    HT = 10
+    maxHP = ST
+    tempHP = maxHP
+
 def roll(skill):
     y = 0
     i = 0
+    result = ""
     for i in range(3):
         x = random.randrange(1,7)
         print(x)
         y += x
         continue
     print("Total = ", y)
-    if y > skill: print("Test failed.")
+    if y > skill:
+        print("Test failed.")
+        result = 1
     elif y == 4:
         print("Automatic Success!")
         result = 2
@@ -41,17 +62,21 @@ def rollDmg(dice, modifier):
     else:
         total = y + modifier
     print("Total Damage = ", total)
+    return total
 
     
-def attack(skill):
+def attack(skill, target):
     result = roll(skill)
+    dmg = 0
     if result == 2:
-        rollDmg(1,-2)
+        dmg = rollDmg(1,-2)
     elif result == 3:
-        print("Total Damage = ", 4)
+        dmg = 4
+        print("Total Damage = ", dmg)
     elif result == 1:
         print("Whiff! No damage dealt.")
     elif result == 0:
-        print("Eventually, we'll have a crit fail chart for this.", \
-                "For now, you just suck.")
-        
+        print("""Eventually, we'll have a crit fail chart for this.
+        For now, you just suck.""")
+    target.tempHP -= dmg
+    print(f"{target.name} has {target.tempHP} HP remaining.")
